92561c89354b01db39a7785fb061eadec629e71f
yasoobkamili <kamiliyasoob@gmail.com>
2025-08-14 12:20:56 +0530

    added loader

--- script.js
+++ script.js
@@ -4,29 +4,10 @@ function toggleSidebar() {
   sidebar.classList.toggle("close");
   toggleButton.classList.toggle("open");
 }
-// window.addEventListener("load", function () {
-//   document.getElementById("loadingOverlay").style.opacity = "0";
-//   setTimeout(function () {
-//     document.getElementById("loadingOverlay").style.display = "none";
-//     document.querySelector(".content").classList.add("loaded");
-//   }, 500);
-// });
-// Record the time when the page starts loading
-const loaderStartTime = Date.now();
-
 window.addEventListener("load", function () {
-  const elapsed = Date.now() - loaderStartTime; // time taken so far
-  const minDisplay = 1500; 
-
-  // Time still left to reach minDisplay
-  const delay = Math.max(0, minDisplay - elapsed);
-
+  document.getElementById("loadingOverlay").style.opacity = "0";
   setTimeout(function () {
-    document.getElementById("loadingOverlay").style.opacity = "0";
-
-    setTimeout(function () {
-      document.getElementById("loadingOverlay").style.display = "none";
-      document.querySelector(".content").classList.add("loaded");
-    }, 500); 
-  }, delay);
+    document.getElementById("loadingOverlay").style.display = "none";
+    document.querySelector(".content").classList.add("loaded");
+  }, 500);
 });
